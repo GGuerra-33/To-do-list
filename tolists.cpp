ab6186661fd9d290da7a2b23237dea60914c6717
Guillermo Guerra <guiguri09@gmail.com>
2024-05-05 00:24:19 +0200

    Creating functions for displaying the menu and options

--- tolists.cpp
+++ tolists.cpp
@@ -1,48 +1,60 @@
 #include <iostream>
+#include <list>
 #include <string>
 
+
+void DisplayMenu (){
+  std::cout << "Menu:\n";
+  std::cout << "1. Create a new task" << std::endl;
+  std::cout << "2. Mark a task as completed" << std::endl;
+  std::cout << "3. Show tasks" << std::endl;
+  std::cout << "4. Exit" << std::endl;
+
+}
+
+void PerformAction(int choice){
+  switch (choice)
+  {
+  case 1:
+    std::cout << "You chose option number " << choice << std::endl;
+  
+  case 2:
+    std:: cout << "You chose option number " << choice << std::endl;
+  default:
+    std::cout << "Invalid option" << std::endl;
+  }
+}
+
+
+
+
 int main(){
-    std::string tasks = {};
-    int options[3] = {1, 2, 3};
-    std::string new_task = {};
 
-    std::cout << "What do you want to do?" << std::endl;
+  int choice;
+  
+  std::list<std::string> tasks;
+  std::string task;
+
+  do
+  {
+    DisplayMenu();
+
+    std::cout << "Select an option: ";
+    std::cin >> choice;
+  
+    PerformAction(choice);
+  
+  } while (choice != 4);
+  
+
+  if (choice == 1){
+    std::cout << "Enter a task: ";
+    std::cin >> task;
     
-    for (int i=0; i>3; ++i){
-        std::cout << options[i] << std::endl;
-    }
+  }
 
-    int selected_option;
-    std::cin >> selected_option;
 
-    std::string new_task = {};
 
-    if (selected_option == 1){
-        std::cout << "Create a new task:" << std::endl;
-        std::getline(std::cin, new_task) /* users writes a task */;
-        tasks.append(new_task);   // Task is appended to the list "tasks"
-        std::cout << tasks << std::endl;
-
-    }
-
-    /* If loop for option 2
-    user is asked which task wants to mark as completed
-    then taks is deleted from the list "tasks"*/
-
-    /*If loop for option 3
-    user is asked if he wants to leave the program
-    if yes, terminate process
-    if not, bring back to initial menu*/
-
-    /*If loop for option 4
-    user visualizes all of this tasks
-    then the user is asked if he wants to terminate the program or to return to the menu
-    if he wants to terminate, then, program.exit()
-    if not, return to initial menu*/
-
-    //Consider changing option 4 for option 2/3
-    //Create a decent debugging environment
-
-    return 0;
+  return 0;
 
 }
